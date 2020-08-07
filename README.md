4ca6249c606565faadd9d32b527ade0196ddb33b
lhanano <lmhanano@gmail.com>
2020-08-06 20:14:41 -0700

    calculator v3.0 new CSS all JS functions work

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # calculator
 
 Objective: Build an on-screen calculator using JavaScript, HTML, and CSS
+
+improvements to be done: 
+- numbers flow outside of display box
+- keyboard clicks do not press button down
--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-// function for main operator aka calulator 
+// CALCULATING RESULTS
 const operate = (num1, operator, num2) => {
     let result;
     switch (operator) {
-        case '+': result = add(Number(num1), Number(num2));
+        case '+': result = Number(num1) + Number(num2);
         break;
-        case '-': result = subtract(Number(num1), Number(num2));
+        case '-': result = Number(num1) - Number(num2);
         break;
-        case 'x': result = multiply(Number(num1), Number(num2));
+        case 'x': result = Number(num1) * Number(num2);
         break;
-        case '÷': result = divide(Number(num1), Number(num2));
+        case '÷': result = Number(num1) / Number(num2);
+        case '%': result = ((Number(num1) * 0.01) * (Number(num2)));
     }
     return result;
 };
 
-// functions for basic math operators
-function add (a, b) {
-	return a + b;	
-}
-
-function subtract (a, b) {
-	return a - b;
-}
-
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a,b) {
-    return a / b;
-}
-
-function power(a, b) {
-	return a**b;
-}
-
-//functions for buttons
+//  HANDLING INPUTS & BUTTON FUNCTIONS
 
 let num1 = '';
 let num2 = '';
@@ -49,21 +28,22 @@ const numberButtons = document.querySelectorAll('.numbers');
 const operandButtons = document.querySelectorAll('.operands');
 const equalsButton = document.querySelector('#equals');
 const clearButton = document.querySelector('#clear');
-const deleteButton = document.querySelector('#delete');
-
 
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         if (operator.length<1 && previousValue.innerText.length<1) {
-        num1 += button.innerText;
-        currentValue.innerText = num1
+            if (button.innerText == '.' && num1.includes('.')) return;
+            num1 += button.innerText;
+            currentValue.innerText = num1
         }
         if (operator.length==1) {
+            if (button.innerText == '.' && num2.includes('.')) return;
             num2 += button.innerText;
             currentValue.innerText = num1 + ' ' + operator + ' ' + num2;
             return
         }
         if (previousValue.innerText.includes('=')) {
+            if (button.innerText == '.' && num1.includes('.')) return;
             previousValue.innerText = previousAnswer;
             num1 += button.innerText;
             currentValue.innerText = num1;
@@ -106,21 +86,29 @@ clearButton.addEventListener("click", () => {
     currentValue.innerText = '';
 })
 
-deleteButton.addEventListener("click", () => {
-    let str = currentValue.innerText;
-        let str2 = str.slice(0, str.length-1);
-        currentValue.innerText = str2;
-        return;
+
+//  KEYBOARD USAGE
+document.addEventListener("keydown", (e) => {
+    if (e.key == '=' || e.key == 'Enter') {
+        equalsButton.click()
+    } if (e.key == 'Escape' || e.key == 'Backspace') {
+        clearButton.click()
+    } if (e.key <= 9 && e.key >= 0) {
+        document.querySelector(`[id='${e.key}']`).click()
+    } if (e.key == '.') {
+        document.querySelector('#decimal').click()
+    } if (e.key == '÷' || e.key == '/') {
+        document.querySelector('#divide').click()
+    } if (e.key == '+') {
+        document.querySelector('#plus').click()
+    } if (e.key == '-') {
+        document.querySelector('#minus').click()
+    } if (e.key == '*' || e.key == 'x' || e.key == 'X') {
+        document.querySelector('#multiply').click()
+    } if (e.key == '÷' || e.key == '/') {
+        document.querySelector('#divide').click()
+    } if (e.key == '%') {
+        document.querySelector('#percent').click()
+    } else return
 })
 
-
-//function for keyboard
-/*
-window.addEventListener("keydown", (e) => {keyPress(e)})
-
-function keyPress(e) {
-    const button = document.querySelector(`#${e.keyCode}`);
-    if (!button) return;
-    button.classList.add("buttonDown");
-}
-*/
